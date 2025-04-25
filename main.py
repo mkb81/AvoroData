60ebdbc874b40f98c8bc79cda33330fd9d0e2379
Marc Kevin Bendig <mkbendig@gmail.com>
2025-04-25 22:12:20 +0200

    Clean code and add .vscode folder

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 """
 Avro example to serialize and deserialize user data
 """
+
 import json
 from io import BytesIO
-from typing import Any, AnyStr, Dict, List
+from typing import Any
 
 import avro
-import avro.io
-from avro.io import BinaryEncoder, BinaryDecoder, DatumWriter, DatumReader
+import avro.schema
+from avro.io import BinaryDecoder, BinaryEncoder, DatumReader, DatumWriter
 from avro.schema import Schema
 
 
-def open_user_data() -> List[dict]:
+def open_user_data() -> list[dict]:
+    """Open user data file
+
+    :return: User data object
+    """
     with open(file="resources/user.json", mode="r") as file:
         return json.load(fp=file)
 
 
 def get_schema_object() -> Schema:
     with open(file="resources/user.avsc", mode="r") as file:
-        schema_data: AnyStr = file.read()
+        schema_data: str = file.read()
 
     return avro.schema.parse(json_string=schema_data)
 
 
 def avro_serialize(user_data: dict, avro_schema: Schema) -> bytes:
+    """Avro serialize user data
+
+    :param user_data: Given user data
+    :param avro_schema: Avro schema content
+    :return: Serialized bytes
+    """
     bytes_writer: BytesIO = BytesIO()
     encoder: BinaryEncoder = BinaryEncoder(writer=bytes_writer)
     writer: DatumWriter = DatumWriter(writers_schema=avro_schema)
@@ -31,7 +42,13 @@ def avro_serialize(user_data: dict, avro_schema: Schema) -> bytes:
     return bytes_writer.getvalue()
 
 
-def avro_deserialize(raw_bytes: bytes, avro_schema: Schema) -> Dict[str, Any]:
+def avro_deserialize(raw_bytes: bytes, avro_schema: Schema) -> dict[str, Any]:
+    """Avro deserialize user data
+
+    :param raw_bytes: Avro serialized bytes
+    :param avro_schema: Avro schema content
+    :return: Deserialize data
+    """
     bytes_reader: BytesIO = BytesIO(initial_bytes=raw_bytes)
     decoder: BinaryDecoder = BinaryDecoder(reader=bytes_reader)
     reader: DatumReader = DatumReader(writers_schema=avro_schema)
@@ -40,12 +57,13 @@ def avro_deserialize(raw_bytes: bytes, avro_schema: Schema) -> Dict[str, Any]:
 
 
 def main() -> None:
-    serialize_data: List[bytes] = list()
+    """Entry point"""
+    serialize_data: list[bytes] = list()
 
     actual_schema: Schema = get_schema_object()
     print(f"Avro schema: {actual_schema}")
 
-    actual_data: List[dict] = open_user_data()
+    actual_data: list[dict] = open_user_data()
     print(f"User data: {actual_data}")
 
     for entry in actual_data:
@@ -58,5 +76,5 @@ def main() -> None:
         print(f"Deserialize: {deserialize_data}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
